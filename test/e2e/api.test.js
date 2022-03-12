5ebc5df2b7270e6f21f9a5438931a96294d98f17
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-03-12 12:39:24 -0300

    test: testando outras rotas

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -8,13 +8,33 @@ import superTest from 'supertest';
 import Server from '../../src/server.js';
 
 describe('API E2E Test Suite', () => {
+  
   test('GET / - should return array', async () => {
     const response = await superTest(Server)
-      .get('/');
+    .get('/');
     
     const data = JSON.parse(response.text);
     expect(data).toBeInstanceOf(Array);
+    expect(data.length).toEqual(0);
   });
-  test.todo('POST / - should save an item and return ok');
-  test.todo('DELETE / - should save  array');
+
+  test('POST / - should save an item and return ok', async () => {
+    const response = await superTest(Server)
+      .post('/')
+      .send({
+        nome: 'marcosrodrigues',
+        age: 21
+      });
+  
+    const expectedResponse = { ok: 1 };
+    expect(JSON.parse(response.text)).toStrictEqual(expectedResponse);
+  }); 
+
+  test('DELETE / - should clear database', async () => {
+    const response = await superTest(Server)
+      .delete('/');
+  
+    const expectedResponse = { ok: 1 };
+    expect(JSON.parse(response.text)).toStrictEqual(expectedResponse);
+  });
 });
