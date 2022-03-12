856fadc81735bccb50f5c85c630589aff94bf246
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-03-12 11:33:37 -0300

    feat: primeiras rotas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,30 @@
 import { createServer } from 'http';
+import { once } from 'events';
+import { randomUUID } from 'crypto';
 
-async function handler(req, res) {
-  res.end('hello world');
+const Database = new Map();
+
+const responseJSON = (data, response) => response.end(JSON.stringify(data))
+
+async function handler(request, response) {
+  const { method } = request;
+
+  if (method === "GET") {
+    return responseJSON([...Database.values()], response);
+  }
+
+  if (method === "POST") {
+    const body = JSON.parse(await once(request, 'data'));
+    console.log('recebido', body);
+    
+    const id = randomUUID();
+    Database.set(id, body);
+    return responseJSON({ ok: 1 }, response);
+  }
+
+  if (method === "DELETE") {
+    return responseJSON({ ok: 1 }, response);
+  }
 }
 
 export default createServer(handler);
